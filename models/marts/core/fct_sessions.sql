b135ef6782fbca1234692bcbd8f1053d0a4d8c68
Toni Ruokolainen <developer@arvoan.com>
2026-02-09 14:26:31 +0200

    Add pages data.

--- models/marts/core/fct_sessions.sql
+++ models/marts/core/fct_sessions.sql
@@ -4,24 +4,46 @@
     partition_by={'field': 'session_start_at', 'data_type': 'timestamp'}
 ) }}
 
-WITH tracks_session_flags AS (
+WITH tracks AS (
     SELECT
         user_id,
         account_id,
-        event_at as occurred_at,
+        event_at as occurred_at
+    FROM {{ ref('stg_segment_tracks') }}
+    {% if is_incremental() %}
+    WHERE event_at >= CAST((SELECT {{ dbt.dateadd('minute', -var('session_timeout_minutes', 30), 'MAX(session_end_at)') }} FROM {{ this }}) AS TIMESTAMP)
+    {% endif %}
+),
+
+pages AS (
+    SELECT
+        user_id,
+        account_id,
+        page_viewed_at as occurred_at
+    FROM {{ ref('stg_segment_pages') }}
+    {% if is_incremental() %}
+    WHERE page_viewed_at >= CAST((SELECT {{ dbt.dateadd('minute', -var('session_timeout_minutes', 30), 'MAX(session_end_at)') }} FROM {{ this }}) AS TIMESTAMP)
+    {% endif %}
+),
+
+combined_events AS (
+    SELECT * FROM tracks
+    UNION ALL
+    SELECT * FROM pages
+),
+
+tracks_session_flags AS (
+    SELECT
+        user_id,
+        account_id,
+        occurred_at,
         -- Generate a session flag: 1 if new session, 0 if same session
         CASE
-            WHEN {{ dbt.datediff('LAG(event_at) OVER (PARTITION BY user_id ORDER BY event_at)', 'event_at', 'minute') }} > {{ var('session_timeout_minutes', 30) }} 
+            WHEN {{ dbt.datediff('LAG(occurred_at) OVER (PARTITION BY user_id ORDER BY occurred_at)', 'occurred_at', 'minute') }} > {{ var('session_timeout_minutes', 30) }} 
             THEN 1
             ELSE 0 
         END AS is_new_session_flag
-    FROM {{ ref('stg_segment_tracks') }}
-    {% if is_incremental() %}
-    -- Only process new data to save costs
-    -- Lookback from last session end to catch sessions that might still be active
-    -- This prevents splitting sessions across incremental runs
-    WHERE event_at >= CAST((SELECT {{ dbt.dateadd('minute', -var('session_timeout_minutes', 30), 'MAX(session_end_at)') }} FROM {{ this }}) AS TIMESTAMP)
-    {% endif %}
+    FROM combined_events
 ),
 
 session_grouping_base AS (
